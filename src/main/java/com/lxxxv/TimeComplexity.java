8c7d82955c77b86057c5dd9fb45563642c0c4387
lxxxv <justcodeit.py@gmail.com>
2022-03-29 23:22:49 +0900

    -

--- src/main/java/com/lxxxv/TimeComplexity.java
+++ src/main/java/com/lxxxv/TimeComplexity.java
@@ -15,7 +15,26 @@ public class TimeComplexity
 
     public static void main(String args[]) throws Exception
     {
-        System.out.println("======================================================");
+        /*
+         * ============================== HOW TO RUN THIS TEST: ====================================
+         *
+         * You are expected to see the run with large number of iterations, and
+         * very large throughput numbers. You can see that as the estimate of the
+         * harness overheads per method call. In most of our measurements, it is
+         * down to several cycles per call.
+         *
+         * a) Via command-line:
+         *    $ mvn clean install
+         *    $ java -jar target/benchmarks.jar TimeComplexity
+         *
+         * JMH generates self-contained JARs, bundling JMH together with it.
+         * The runtime options for the JMH are available with "-h":
+         *    $ java -jar target/benchmarks.jar -h
+         *
+         * b) Via the Java API:
+         *    (see the JMH homepage for possible caveats when running from IDE:
+         *      http://openjdk.java.net/projects/code-tools/jmh/)
+         */
 
         Options opt = new OptionsBuilder()
                 .include(TimeComplexity.class.getSimpleName())
