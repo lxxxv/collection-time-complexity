75e43fbf712bc1db3c6a6e1020b42aef14d24083
lxxxv <justcodeit.py@gmail.com>
2022-03-30 16:59:14 +0900

    -

--- src/main/java/com/lxxxv/TimeComplexityList.java
+++ src/main/java/com/lxxxv/TimeComplexityList.java
@@ -1,6 +1,7 @@
 package com.lxxxv;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
@@ -8,12 +9,15 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.util.concurrent.TimeUnit;
 import java.util.*;
 
+// http://tutorials.jenkov.com/java-performance/jmh.html
+// https://ysjee141.github.io/blog/quality/java-benchmark/
+
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class TimeComplexityList
 {
-    public int LOOP_COUNT = 100000;
+    public int LOOP_COUNT = 1000;
 
     Random rm;
 
@@ -41,107 +45,128 @@ public class TimeComplexityList
     }
 
     @Benchmark
-    public void addArrayList()
+    public void addArrayList(Blackhole bl)
     {
         benchList = new ArrayList<>();
+        String result;
         for (int loop = 0; loop < LOOP_COUNT; loop++)
         {
-            benchList.add(Integer.toString(rm.nextInt()) + "_" + Integer.toString(loop));
+            result = Integer.toString(rm.nextInt()) + "_" + Integer.toString(loop);
+            benchList.add(result);
+            bl.consume(result);
         }
     }
 
     @Benchmark
-    public void addLinkedList()
+    public void addLinkedList(Blackhole bl)
     {
         benchList = new LinkedList<>();
+        String result;
         for (int loop = 0; loop < LOOP_COUNT; loop++)
         {
-            benchList.add(Integer.toString(rm.nextInt()) + "_" + Integer.toString(loop));
+            result = Integer.toString(rm.nextInt()) + "_" + Integer.toString(loop);
+            benchList.add(result);
+            bl.consume(result);
         }
     }
 
     @Benchmark
-    public void addVector()
+    public void addVector(Blackhole bl)
     {
         benchList = new Vector<>();
+        String result;
         for (int loop = 0; loop < LOOP_COUNT; loop++)
         {
-            benchList.add(Integer.toString(rm.nextInt()) + "_" + Integer.toString(loop));
+            result = Integer.toString(rm.nextInt()) + "_" + Integer.toString(loop);
+            benchList.add(result);
+            bl.consume(result);
         }
     }
 
     @Benchmark
-    public void getArrayList()
+    public void getArrayList(Blackhole bl)
     {
         String result;
         for (int loop = 0; loop < LOOP_COUNT; loop++)
         {
             result = benchArrayList.get(loop);
+            bl.consume(result);
         }
     }
 
     @Benchmark
-    public void getLinkedList()
+    public void getLinkedList(Blackhole bl)
     {
         String result;
         for (int loop = 0; loop < LOOP_COUNT; loop++)
         {
             result = benchLinkedList.get(loop);
+            bl.consume(result);
         }
     }
 
     @Benchmark
-    public void getVector()
+    public void getVector(Blackhole bl)
     {
         String result;
         for (int loop = 0; loop < LOOP_COUNT; loop++)
         {
             result = benchVector.get(loop);
+            bl.consume(result);
         }
     }
 
     @Benchmark
-    public void sortArrayList()
+    public void sortArrayList(Blackhole bl)
     {
+        bl.consume(0);
         Collections.sort(benchArrayList);
+        bl.consume(benchArrayList.size());
     }
 
     @Benchmark
-    public void sortLinkedList()
+    public void sortLinkedList(Blackhole bl)
     {
+        bl.consume(0);
         Collections.sort(benchLinkedList);
+        bl.consume(benchLinkedList.size());
     }
 
     @Benchmark
-    public void sortVector()
+    public void sortVector(Blackhole bl)
     {
+        bl.consume(0);
         Collections.sort(benchVector);
+        bl.consume(benchVector.size());
     }
 
     @Benchmark
-    public void delArrayList()
+    public void delArrayList(Blackhole bl)
     {
-        for (int loop = 0; loop < LOOP_COUNT; loop++)
+        while(benchArrayList.size() > 0)
         {
-            benchArrayList.remove(loop);
+            benchArrayList.remove(0);
+            bl.consume(benchArrayList.size());
         }
     }
 
     @Benchmark
-    public void delLinkedList()
+    public void delLinkedList(Blackhole bl)
     {
-        for (int loop = 0; loop < LOOP_COUNT; loop++)
+        while(benchLinkedList.size() > 0)
         {
-            benchLinkedList.remove(loop);
+            benchLinkedList.remove(0);
+            bl.consume(benchLinkedList.size());
         }
     }
 
     @Benchmark
-    public void delVector()
+    public void delVector(Blackhole bl)
     {
-        for (int loop = 0; loop < LOOP_COUNT; loop++)
+        while(benchVector.size() > 0)
         {
-            benchVector.remove(loop);
+            benchVector.remove(0);
+            bl.consume(benchVector.size());
         }
     }
 
