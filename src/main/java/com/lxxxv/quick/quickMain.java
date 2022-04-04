63c5c89a1338dfb22b5fe94ce652286dd46e4707
lxxxv <justcodeit.py@gmail.com>
2022-04-04 09:39:12 +0900

    -

--- src/main/java/com/lxxxv/quick/quickMain.java
+++ src/main/java/com/lxxxv/quick/quickMain.java
@@ -21,8 +21,7 @@ public class quickMain
         new MapDel().execute();
         new MapGet().execute();
 
-//        new QueueAdd().execute();
-//        new QueueDel().execute();
-//        new QueueGet().execute();
+        new QueueAdd().execute();
+        new QueueGet().execute();
     }
 }
